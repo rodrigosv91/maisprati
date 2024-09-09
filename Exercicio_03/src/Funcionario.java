c7765d49375b9a96f9dd9faf1b8690523d717d77
rodrigo <rodrigosv91@gmail.com>
2024-09-08 22:10:39 -0300

    add: exercicio 03

---
+++ Exercicio_03/src/Funcionario.java
@@ -0,0 +1,23 @@
+public class Funcionario {
+    // Atributos protegidos
+    protected String nome;
+    protected double salario;
+
+    // Construtor da classe base
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    // Método para calcular bônus (a ser sobrescrito nas subclasses)
+    public double calcularBonus() {
+        return 0; // Sem bônus na classe base
+    }
+
+    // Método para exibir as informações do funcionário
+    public void exibirInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Salário: " + salario);
+        System.out.println("Bônus: " + calcularBonus());
+    }
+}
---
+++ Exercicio_03/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Criando um objeto Gerente
+        Gerente gerente = new Gerente("Alice", 8000.00);
+        gerente.exibirInformacoes();
+
+        // Criando um objeto Desenvolvedor
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Bob", 5000.00);
+        desenvolvedor.exibirInformacoes();
+    }
+}
